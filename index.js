65aba4ee24f97737e5c2b88b2fdbf1b459f48cb7
nettenegri <nettenegri@gmail.com>
2023-02-25 19:37:53 -0700

    commit tree 22655ef2daf632d56c251844fd23860cf322ff50

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(firstActivity = "roller-skate") {
+  console.log(`This Saturday, I want to ${firstActivity}!`);
+  return `This Saturday, I want to ${firstActivity}!`;
+}
+
+saturdayFun();
+saturdayFun("bathe my dog");
+
+const mondayWork = function (secondActivity = "go to the office") {
+  console.log(`This Monday, I will ${secondActivity}.`);
+  return `This Monday, I will ${secondActivity}.`;
+};
+mondayWork();
+
+function wrapAdjective(outterFunction = "*") {
+  return function (innerFunction = "special") {
+    return `You are ${outterFunction}${innerFunction}${outterFunction}!`;
+  };
+}
+wrapAdjective("*")("a hard worker!");
